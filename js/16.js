258c18b075f7ee22b3cde78bca1ac2218173b4cf
Efraska <aux_sistemas@medicainsurgentes.com.mx>
2024-08-09 18:00:47 -0600

    Arrow functions y methods

---
+++ js/16.js
@@ -0,0 +1,20 @@
+// Funciones en JavaScript
+
+// Declaracion de la funcion
+function sumar() {
+    console.log(10 + 10);
+}
+
+sumar();
+
+// Expresión de la función
+const sumar2 = function() {
+    console.log(3 + 3);
+}
+
+sumar2();
+
+// // IIFE
+// (function() {
+//     console.log('Esto es una función');
+// })();
